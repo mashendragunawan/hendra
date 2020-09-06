adb8267365a4145644bd8afd093679bef9f636a4
mashendragunawan04@gmail.com <mashendragunawan04@gmail.com>
2020-09-06 11:23:12 +0700

    hendra

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'hello world'),
+      home: MyHomePage(title: 'Mas Hendra Gunawan'),
     );
   }
 }
