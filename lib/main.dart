9b55bcac46476489862821beaa792d1357119ae6
mashendragunawan04@gmail.com <mashendragunawan04@gmail.com>
2020-09-06 11:30:35 +0700

    ujang

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Mas Hendra Gunawan'),
+      home: MyHomePage(title: 'UJANG AJEJEJ'),
     );
   }
 }
